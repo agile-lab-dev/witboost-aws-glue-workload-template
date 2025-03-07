4bc90561895439b17a374280310cc60313b48dfb
Carlo Ventrella <carlo.ventrella@agilelab.it>
2025-03-07 13:57:18 +0000

    [EUR-51] Glue fix bucket computation in ci cd
    # New features and improvements
    
    This MR changes the way the bucket name is computed in the ci/cd template. It follows the logic of the tech adapter.
    
    # Related issue
    
    Closes EUR-51

--- skeleton/ci-utils/compute-bucket-name.sh
+++ skeleton/ci-utils/compute-bucket-name.sh
@@ -2,10 +2,11 @@
 
 compute_bucket_name() {
     local domain="$1"
-    local name="$2"
-    local environment="$3"
+    local dpName="$2"
+    local componentName= "$3"
+    local environment="$4"
 
-    local bucket_name_without_hash="${domain}-${name}-${environment}"
+    local bucket_name_without_hash="${domain}-${dpName}-${componentName}-${environment}"
     bucket_name_without_hash=$(echo "$bucket_name_without_hash" | tr -d ' ' | tr '[:upper:]' '[:lower:]')
 
     local hash=$(echo -n "$bucket_name_without_hash" | sha256sum | cut -c1-5)
